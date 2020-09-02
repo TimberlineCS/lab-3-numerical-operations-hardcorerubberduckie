be42301cf4016be0572c4d7720a34ade27302147
OmerNomer <hardcorerubberduckie@gmail.com>
2020-09-02 05:06:39 +0000

    did challenge 1

---
+++ NumberOps.java
@@ -0,0 +1,46 @@
+public class NumberOps{
+
+public static void main(String args []){
+
+double d1 = 37.9;
+double d2 = 1004.128;
+int i1 = 12;
+int i2 = 18;
+
+double Q;
+double W;                                                   //Challenge 1
+double E;                                                   //Dmitri Albig, Computer Science
+double R;
+double T;
+  
+  Q = 57.2 * (i1 / i2) + 1;
+  System.out.println(Q);
+
+W = 57.2 * ((double)i1 / i2) + 1;
+System.out.println(W);
+
+E = 15 - i1 * (d1 * 3) + 4;
+System.out.println(E);
+
+R = 15 - i1 * ((int)d1 * 3) + 4;
+System.out.println(R);
+
+T = 15 - i1 * (int)(d1 * 3) + 4;
+System.out.println(T);
+
+//____________________________________________________________________________________________________________________________
+
+// challenge 2
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
